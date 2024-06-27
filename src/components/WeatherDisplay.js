b807ce8a9ea9ca7608bc52f8953073bea273b154
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-06-27 12:01:10 +0530

    submission3

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -2,9 +2,10 @@ import React from "react";
 
 const WeatherDisplay = ({temperature,condition})=>{
     return <>
-        <span style={{color: `${temperature<20?"blue":"red"}`}}>Temperature: {temperature}</span>
-        <br/>
-        <span>Condition: {condition}</span>
+        <span style={{color: `${temperature<20?"blue":"red"}`}}>
+            <p>Temperature: {temperature}</p>
+            <p>Condition: {condition}</p>
+        </span>
     </>
 }
 export default WeatherDisplay;
